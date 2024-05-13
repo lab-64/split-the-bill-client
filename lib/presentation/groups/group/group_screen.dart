870adfa9f5039eb4faa5099e573378abb34a4b5b
GR0ZA <grzegorz.rolka8@gmail.com>
2024-05-13 19:46:06 +0200

    #82 Fix merge conflict

--- lib/presentation/groups/group/group_screen.dart
+++ lib/presentation/groups/group/group_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:split_the_bill/constants/app_sizes.dart';
 import 'package:split_the_bill/domain/group/states/group_state.dart';
 import 'package:split_the_bill/domain/group/states/groups_state.dart';
+import 'package:split_the_bill/infrastructure/async_value_ui.dart';
 import 'package:split_the_bill/presentation/groups/group/group_members.dart';
 import 'package:split_the_bill/presentation/shared/async_value_widget.dart';
 import 'package:split_the_bill/presentation/shared/bills/bills_list.dart';
@@ -62,9 +63,8 @@ class GroupScreen extends ConsumerWidget {
                 PopupMenuButton(
                   itemBuilder: (context) => [
                     PopupMenuItem(
-                      onTap: () {
-                        EditGroupRoute(groupId: groupId).push(context),
-                      },
+                      onTap: () =>
+                          EditGroupRoute(groupId: groupId).push(context),
                       child: const Row(
                         children: [
                           Icon(
