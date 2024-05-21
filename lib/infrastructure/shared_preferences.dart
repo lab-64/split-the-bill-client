8e1fd90ed5730b4768882b1fb8ee78e1bf4c804e
GR0ZA <grzegorz.rolka8@gmail.com>
2024-05-21 22:27:21 +0200

    #97 Add comments explaining the shared_preferences.dart structure

--- lib/infrastructure/shared_preferences.dart
+++ lib/infrastructure/shared_preferences.dart
@@ -5,13 +5,21 @@ part 'shared_preferences.g.dart';
 
 const String _userKey = 'USER';
 
+/// A Riverpod provider for SharedPreferences.
+/// It throws an [UnimplementedError] to indicate that it should be overridden in the main function
+/// with the actual instance of SharedPreferences. This is done to ensure that SharedPreferences
+/// is properly initialized before being used.
 @Riverpod(keepAlive: true)
 SharedPreferences sharedPreferences(SharedPreferencesRef ref) {
   throw UnimplementedError();
 }
 
+/// A Riverpod provider for SharedUtility.
+/// This provider depends on the [sharedPreferencesProvider] to get an instance of SharedPreferences.
+/// The [SharedUtility] class provides convenient methods to interact with SharedPreferences.
 @Riverpod(keepAlive: true)
 SharedUtility sharedUtility(SharedUtilityRef ref) {
+  // Watch the sharedPreferencesProvider to get the SharedPreferences instance
   final sharedPrefs = ref.watch(sharedPreferencesProvider);
   return SharedUtility(sharedPreferences: sharedPrefs);
 }
