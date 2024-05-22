cfd75f81e866b3d7e415d29189e15e9e67bde22f
GR0ZA <grzegorz.rolka8@gmail.com>
2024-05-23 01:02:20 +0200

    #100 Merge conflict, add pull to refresh to transactions_screen.dart

--- lib/presentation/transactions/transactions_screen.dart
+++ lib/presentation/transactions/transactions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:split_the_bill/domain/group/states/groups_state.dart';
 import 'package:split_the_bill/domain/group/states/groups_transaction_state.dart';
 import 'package:split_the_bill/presentation/shared/async_value_widget.dart';
 import 'package:split_the_bill/presentation/transactions/transactions_list.dart';
@@ -16,16 +17,20 @@ class TransactionScreen extends ConsumerWidget {
       appBar: AppBar(
         title: const Text("Transactions"),
       ),
-      body: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: AsyncValueWidget(
+      body: RefreshIndicator(
+        onRefresh: () => ref.refresh(groupsTransactionStateProvider.future),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: AsyncValueWidget(
                 value: transactions,
                 data: (transactions) => TransactionsList(
                     transactions: transactions,
-                    scrollController: scrollController)),
-          ),
-        ],
+                    scrollController: scrollController),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
