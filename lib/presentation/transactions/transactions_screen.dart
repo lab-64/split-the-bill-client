17c57c9630d512fcf509964bd65193a284a09bad
GR0ZA <grzegorz.rolka8@gmail.com>
2024-05-23 17:18:04 +0200

    #100 Remove not used import

--- lib/presentation/transactions/transactions_screen.dart
+++ lib/presentation/transactions/transactions_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:split_the_bill/domain/group/states/groups_state.dart';
 import 'package:split_the_bill/domain/group/states/groups_transaction_state.dart';
 import 'package:split_the_bill/presentation/shared/async_value_widget.dart';
 import 'package:split_the_bill/presentation/transactions/transactions_list.dart';
