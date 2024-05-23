1aa13c8cd42ff0edd800da0c16969f3f76c56b2c
Marvin <marvi1999@mx.de>
2024-05-23 17:17:29 +0200

    #143 only portrait mode allowed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:split_the_bill/app.dart';
@@ -8,6 +9,8 @@ import 'package:split_the_bill/infrastructure/shared_preferences.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final sharedPreferences = await SharedPreferences.getInstance();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
   runApp(
     ProviderScope(
