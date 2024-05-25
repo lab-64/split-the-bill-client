cd84ddc91c11c503773e40885370ffbbb3652fac
GR0ZA <grzegorz.rolka8@gmail.com>
2024-05-25 22:41:47 +0200

    #150 Remove not used code

--- lib/presentation/groups/groups/groups_screen.dart
+++ lib/presentation/groups/groups/groups_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:split_the_bill/constants/ui_constants.dart';
@@ -62,71 +61,3 @@ class GroupsScreen extends ConsumerWidget {
     );
   }
 }
-
-/*
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:split_the_bill/constants/ui_constants.dart';
-import 'package:split_the_bill/domain/group/states/groups_state.dart';
-import 'package:split_the_bill/presentation/groups/groups/join_group_dialog.dart';
-import 'package:split_the_bill/presentation/shared/components/primary_button.dart';
-import 'package:split_the_bill/presentation/shared/groups/groups_list.dart';
-import 'package:split_the_bill/router/routes.dart';
-
-class GroupsScreen extends ConsumerWidget {
-  const GroupsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final ScrollController scrollController = ScrollController();
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Groups"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: Sizes.p24),
-        child: Column(
-          children: [
-            RefreshIndicator(
-              onRefresh: () => ref.refresh(groupsStateProvider.future),
-              child: CustomScrollView(
-                slivers: [
-                  const SliverToBoxAdapter(child: gapH16),
-                  SliverToBoxAdapter(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        PrimaryButton(
-                          onPressed: () => showDialog<void>(
-                            context: context,
-                            builder: (BuildContext context) =>
-                                const JoinGroupDialog(),
-                          ),
-                          icon: Icons.arrow_forward,
-                          backgroundColor: Colors.purple.shade300,
-                          text: "Join Group",
-                        ),
-                        PrimaryButton(
-                          onPressed: () =>
-                              const EditGroupRoute(groupId: '0').push(context),
-                          icon: Icons.add,
-                          backgroundColor: Colors.green.shade300,
-                          text: "Add Group",
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SliverToBoxAdapter(child: gapH16),
-                  GroupsList(scrollController: scrollController),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
- */
