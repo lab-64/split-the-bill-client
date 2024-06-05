689080350af0164e5c4603aa1e6f01da539a509e
Marvin <marvi1999@mx.de>
2024-06-05 15:27:55 +0200

    #180 added a divider by group and a price tag to the notifications

--- lib/presentation/home/notifications_dialog.dart
+++ lib/presentation/home/notifications_dialog.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:split_the_bill/constants/ui_constants.dart';
 import 'package:split_the_bill/domain/bill/states/bills_state.dart';
+import 'package:split_the_bill/domain/group/states/groups_state.dart';
 import 'package:split_the_bill/presentation/shared/async_value_widget.dart';
 import 'package:split_the_bill/presentation/shared/components/date_label.dart';
+import 'package:split_the_bill/presentation/shared/components/fade_text.dart';
+import 'package:split_the_bill/presentation/shared/extensions/currency_formatter.dart';
 import 'package:split_the_bill/presentation/shared/profile/profile_image.dart';
 import 'package:split_the_bill/router/routes.dart';
 
@@ -33,6 +38,7 @@ class _NotificationsDialogState extends ConsumerState<NotificationsDialog> {
   @override
   Widget build(BuildContext context) {
     final bills = ref.watch(billsStateProvider(isUnseen: true));
+    final groups = ref.watch(groupsStateProvider);
 
     return AsyncValueWidget(
       value: bills,
@@ -45,58 +51,79 @@ class _NotificationsDialogState extends ConsumerState<NotificationsDialog> {
         return Column(
           children: [
             const Text(
-              "Tap to confirm your contributions",
+              "Tap or swipe to edit your contributions",
               style: TextStyle(),
             ),
             const Divider(),
             Column(
               children: List.generate(
                 localBills.length,
-                (index) => Dismissible(
-                  key: UniqueKey(),
-                  direction: DismissDirection.horizontal,
-                  onDismissed: (direction) async {
-                    setState(() {
-                      localBills.removeAt(index);
-                    });
-                    if (direction == DismissDirection.startToEnd) {
-                      await _populateContributions(bills[index]);
-                    } else {
-                      await _depopulateContributions(bills[index]);
-                    }
-                  },
-                  background: Container(
-                    color: Colors.green,
-                    alignment: Alignment.centerLeft,
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: const Icon(
-                      Icons.add,
-                      color: Colors.white,
+                (index) => Column(
+                  children: [
+                    if (index == 0 ||
+                        localBills[index - 1].groupId !=
+                            localBills[index].groupId) ...[
+                      gapH8,
+                      FadeText(
+                        text: groups.requireValue
+                            .firstWhere((group) =>
+                                group.id == localBills[index].groupId)
+                            .name,
+                      ),
+                      const Divider()
+                    ],
+                    Dismissible(
+                      key: UniqueKey(),
+                      direction: DismissDirection.horizontal,
+                      onDismissed: (direction) async {
+                        setState(() {
+                          localBills.removeAt(index);
+                        });
+                        if (direction == DismissDirection.startToEnd) {
+                          await _populateContributions(bills[index]);
+                        } else {
+                          await _depopulateContributions(bills[index]);
+                        }
+                      },
+                      background: Container(
+                        color: Colors.green,
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: const Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                      ),
+                      secondaryBackground: Container(
+                        color: Colors.red,
+                        alignment: Alignment.centerRight,
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: const Icon(
+                          Icons.remove,
+                          color: Colors.white,
+                        ),
+                      ),
+                      child: ListTile(
+                        leading: ProfileImage(
+                          user: localBills[index].owner,
+                          size: Sizes.p16,
+                        ),
+                        title: FadeText(
+                          text: localBills[index].name,
+                        ),
+                        subtitle: DateLabel(date: localBills[index].date),
+                        trailing: FadeText(
+                            text: _getPrice(
+                          localBills[index],
+                        )),
+                        onTap: () {
+                          Navigator.of(context).pop();
+                          UnseenBillRoute(billId: localBills[index].id)
+                              .push(context);
+                        },
+                      ),
                     ),
-                  ),
-                  secondaryBackground: Container(
-                    color: Colors.red,
-                    alignment: Alignment.centerRight,
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: const Icon(
-                      Icons.delete,
-                      color: Colors.white,
-                    ),
-                  ),
-                  child: ListTile(
-                    leading: ProfileImage(
-                      user: localBills[index].owner,
-                      size: Sizes.p16,
-                    ),
-                    title: Text(localBills[index].name),
-                    subtitle: DateLabel(date: localBills[index].date),
-                    trailing: const Icon(Icons.arrow_forward),
-                    onTap: () {
-                      Navigator.of(context).pop();
-                      UnseenBillRoute(billId: localBills[index].id)
-                          .push(context);
-                    },
-                  ),
+                  ],
                 ),
               ).toList(),
             ),
@@ -124,5 +151,11 @@ class _NotificationsDialogState extends ConsumerState<NotificationsDialog> {
     }
     await ref.read(billsStateProvider().notifier).edit(bill);
     ref.invalidate(billsStateProvider(isUnseen: true));
+  }
+
+  String _getPrice(Bill bill) {
+    return bill.items
+        .fold(0.0, (prev, element) => prev + element.price)
+        .toCurrencyString();
   }
 }
