72ec358942bae1dc87429332fff5fdbe19c02621
marvin <marvi1999@gmx.de>
2024-06-07 16:09:57 +0200

    Change to release mode

--- lib/presentation/shared/profile/profile_image.dart
+++ lib/presentation/shared/profile/profile_image.dart
@@ -44,7 +44,7 @@ class ProfileImage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final authUser = ref.watch(authStateProvider);
 
-    final String imagePath = user.getImagePath();
+    final String imagePath = user.profileImgPath;
     final ImageProvider<Object> image = getProfileImage(
       previewImage,
       imagePath,
