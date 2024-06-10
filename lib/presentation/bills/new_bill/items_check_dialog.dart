4f55982988eee5d0ceeb60d5339bc3b6fa172f60
Marvin <marvin.christian@stud.tu-darmstadt.de>
2024-06-10 15:10:44 +0200

    fix dart analysis

--- lib/presentation/bills/new_bill/items_check_dialog.dart
+++ lib/presentation/bills/new_bill/items_check_dialog.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:linked_scroll_controller/linked_scroll_controller.dart';
 import 'package:split_the_bill/constants/ui_constants.dart';
