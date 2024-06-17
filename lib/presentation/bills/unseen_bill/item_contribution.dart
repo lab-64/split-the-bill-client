e6c9c7fbb6b867a3665f1a733a4f06027386c639
GR0ZA <grzegorz.rolka8@gmail.com>
2024-06-17 17:44:12 +0200

    #240 Remove unused import

--- lib/presentation/bills/unseen_bill/item_contribution.dart
+++ lib/presentation/bills/unseen_bill/item_contribution.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../../../constants/ui_constants.dart';
 import '../../../domain/bill/item.dart';
-import '../../shared/profile/profile_image.dart';
 
 class ItemContribution extends StatelessWidget {
   const ItemContribution({
