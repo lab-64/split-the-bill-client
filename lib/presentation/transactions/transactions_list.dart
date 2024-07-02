a724fbf6afa0ca4191c376766d015dee569d6d1c
Marvin <marvi1999@mx.de>
2024-07-02 19:22:22 +0200

    #256 added a button to transactions, to only view ones the user is part of

--- lib/presentation/transactions/transactions_list.dart
+++ lib/presentation/transactions/transactions_list.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:split_the_bill/auth/data/auth_repository.dart';
+import 'package:split_the_bill/auth/states/auth_state.dart';
 import 'package:split_the_bill/constants/ui_constants.dart';
 import 'package:split_the_bill/domain/group/group_transaction.dart';
 import 'package:split_the_bill/domain/group/states/groups_transaction_state.dart';
 import 'package:split_the_bill/presentation/shared/components/placeholder_display.dart';
 import 'package:split_the_bill/presentation/transactions/transaction_item.dart';
 
+import '../../domain/group/transaction.dart';
+import '../shared/components/primary_button.dart';
 import '../shared/components/rounded_box.dart';
 import 'transaction_date_tab.dart';
 import 'transaction_group_header.dart';
@@ -25,6 +29,8 @@ class TransactionsList extends ConsumerStatefulWidget {
 class _TransactionListState extends ConsumerState<TransactionsList> {
   late List<bool> transactionsExpanded;
   late List<GroupTransaction> transactions;
+  late List<GroupTransaction> originalTransactions;
+  bool onlyOwn = false;
 
   @override
   void initState() {
@@ -33,7 +39,10 @@ class _TransactionListState extends ConsumerState<TransactionsList> {
   }
 
   void _init(List<GroupTransaction> transactions) {
-    this.transactions = transactions;
+    this.transactions = List.generate(
+        widget.transactions.length, (index) => transactions[index].copyWith());
+    originalTransactions = List.generate(
+        widget.transactions.length, (index) => transactions[index].copyWith());
     transactionsExpanded = List.generate(
         transactions.length,
         (index) => transactions[index] == transactions.last ||
@@ -41,6 +50,23 @@ class _TransactionListState extends ConsumerState<TransactionsList> {
                     transactions[index + 1].groupName
             ? true
             : false);
+  }
+
+  void _filterTransactions() {
+    final user = ref.read(authStateProvider).requireValue;
+    setState(() {
+      onlyOwn = !onlyOwn;
+      if (onlyOwn) {
+        for (var transaction in transactions) {
+          transaction.transactions.removeWhere((transaction) =>
+              transaction.creditor.id != user.id &&
+              transaction.debtor.id != user.id);
+        }
+      } else {
+        transactions = List.generate(originalTransactions.length,
+            (index) => originalTransactions[index].copyWith());
+      }
+    });
   }
 
   @override
@@ -52,57 +78,73 @@ class _TransactionListState extends ConsumerState<TransactionsList> {
       },
     );
 
-    return ListView(
-      controller: widget.scrollController,
-      shrinkWrap: true,
+    return Column(
       children: [
-        if (transactions.isEmpty)
-          const Center(
-            child: Padding(
-              padding: EdgeInsets.all(Sizes.p16),
-              child: PlaceholderDisplay(
-                icon: Icons.currency_exchange,
-                message: "No recent transactions",
-              ),
-            ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: PrimaryButton(
+            onPressed: () => _filterTransactions(),
+            icon: onlyOwn ? Icons.visibility_off : Icons.visibility,
+            backgroundColor: Colors.purple.shade300,
+            text: "Toggle visibility",
           ),
-        for (int i = 0; i < transactions.length; i++) ...[
-          //first in group
-          if (i == 0 ||
-              transactions[i].groupName != transactions[i - 1].groupName)
-            TransactionGroupHeader(transaction: transactions[i]),
-          //first for that date
-          if (i == 0 || transactions[i].date.isAfter(transactions[i - 1].date))
-            TransactionDateTab(
-              index: i,
-              onTap: invertTransactionExpanded,
-              transaction: transactions[i],
-              transactionExpanded: transactionsExpanded[i],
-            ),
-          //if open or last for the group
-          if (transactionsExpanded[i] &&
-              transactions[i].transactions.isNotEmpty) ...[
-            ListView.builder(
-                controller: widget.scrollController,
-                shrinkWrap: true,
-                itemCount: transactions[i].transactions.length,
-                itemBuilder: (context, index) {
-                  final transaction = transactions[i].transactions[index];
-                  return TransactionItem(transaction: transaction);
-                })
+        ),
+        ListView(
+          controller: widget.scrollController,
+          shrinkWrap: true,
+          children: [
+            if (transactions.isEmpty)
+              const Center(
+                child: Padding(
+                  padding: EdgeInsets.all(Sizes.p16),
+                  child: PlaceholderDisplay(
+                    icon: Icons.currency_exchange,
+                    message: "No recent transactions",
+                  ),
+                ),
+              ),
+            for (int i = 0; i < transactions.length; i++) ...[
+              //first in group
+              if (i == 0 ||
+                  transactions[i].groupName != transactions[i - 1].groupName)
+                TransactionGroupHeader(transaction: transactions[i]),
+              //first for that date
+              if (i == 0 ||
+                  transactions[i].date.isAfter(transactions[i - 1].date))
+                TransactionDateTab(
+                  index: i,
+                  onTap: invertTransactionExpanded,
+                  transaction: transactions[i],
+                  transactionExpanded: transactionsExpanded[i],
+                ),
+              //if open or last for the group
+              if (transactionsExpanded[i] &&
+                  transactions[i].transactions.isNotEmpty) ...[
+                ListView.builder(
+                    controller: widget.scrollController,
+                    shrinkWrap: true,
+                    itemCount: transactions[i].transactions.length,
+                    itemBuilder: (context, index) {
+                      final transaction = transactions[i].transactions[index];
+                      return TransactionItem(transaction: transaction);
+                    })
+              ],
+              if (transactionsExpanded[i] &&
+                  transactions[i].transactions.isEmpty)
+                const RoundedBox(
+                    firstPadding: EdgeInsets.only(
+                        left: Sizes.p24,
+                        right: Sizes.p24,
+                        top: Sizes.p4,
+                        bottom: Sizes.p4),
+                    secondPadding: EdgeInsets.all(Sizes.p16),
+                    backgroundColor: Colors.white,
+                    child:
+                        Center(child: Text("No money transactions necessary"))),
+              if (i == transactions.length - 1) ...[gapH24]
+            ]
           ],
-          if (transactionsExpanded[i] && transactions[i].transactions.isEmpty)
-            const RoundedBox(
-                firstPadding: EdgeInsets.only(
-                    left: Sizes.p24,
-                    right: Sizes.p24,
-                    top: Sizes.p4,
-                    bottom: Sizes.p4),
-                secondPadding: EdgeInsets.all(Sizes.p16),
-                backgroundColor: Colors.white,
-                child: Center(child: Text("No money transactions necessary"))),
-          if (i == transactions.length - 1) ...[gapH24]
-        ]
+        ),
       ],
     );
   }
